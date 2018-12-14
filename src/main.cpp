6a8cbb395af3330cb77cff8b8817338690363bd3
zhouwei215664 <zhouwei215664@sogou-inc.com>
2018-12-14 18:57:53 +0800

    review note

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 
 int main() {
     std::cout << "==== main start ====" << std::endl;
-    
+
 #ifdef SWI_ENABLE_MAX
     std::cout << "main moudle SWI_ENABLE_MAX" << std::endl;
 #endif
+
     log("hello Cmakelist");
 
     return 0;
