8c46ebcb706697d11709010dbf1a89b3ae5cf957
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-07-13 18:29:57 +0800

    note

--- src/stl_main.cpp
+++ src/stl_main.cpp
@@ -73,34 +73,68 @@ void test_stl1() {
     *p = 111;
     std::cout << "k: " << k << std::endl;
 
-//    RR rr1;
-//    RR rr2;
+    RR rr1;
+    RR rr2;
 //    RR rr3(test_stl11());
 //    RR rr3(rr1 + rr2);
 //    std::cout << &rr3 << std::endl;
-//    RR rr3(std::move(rr1));
+    std::move(rr1);
+    //RR rr3(std::move(rr1));
 //    RR rr3(rr1);
 
-    RR rr1;
-    rr1.hello = "test";
-    RR rr2 = std::move(rr1);
-    rr1.sysHello();
+//    RR rr1;
+//    rr1.hello = "test";
+//    RR rr2 = std::move(rr1);
+//    rr1.sysHello();
 }
 
-struct X {
+class X {
+public:
     X() { std::cout << "constructed\n"; }
 
     X(const X &) { std::cout << "copied\n"; }
 
-    X(X &&) { std::cout << "moved\n"; }
+//     X(X &&x) = delete;
+//    X(X &&x){
+//        std::cout << "constructed &&\n";
+//    }
+
+    X(X &&x) { std::cout << "moved\n"; }
+
+    X &operator=(const X &&x) {
+        std::cout << "operator\n";
+        return *this;
+    }
 
     ~X() { std::cout << "destructed\n"; }
 };
 
+/**
+ *
+ */
 std::vector<X> Get() {
     // copy elision from tempory object to 'temp', omit copy and move constructor
     std::vector<X> temp = std::vector<X>(1);
     return temp; // NRVO temp to returned
+}
+
+X getX() {
+    // 会默认的调用移动构造函数
+    std::cout << "--------" << std::endl;
+    //
+    X x;
+    std::cout << "+++++++++" << &x << std::endl;
+
+    return x;
+}
+
+std::vector<X> Get1() {
+    std::vector<X> temp = std::vector<X>(1);
+    X x;
+    std::cout << "push_back before" << std::endl;
+    temp.push_back(x);
+    std::cout << "push_back after" << std::endl;
+    return temp;
 }
 
 class RRR {
@@ -119,9 +153,17 @@ void test_stl2() {
     RRR r = test_stl2_1();
 }
 
+void test_stl3() {
+    // 错误用法
+    // 移动构造函数被delete
+//    X x = getX();
+//    std::cout << "main: " << &x << std::endl;
+    X x(getX());
+}
+
 
 int main() {
-    test_stl2();
+    test_stl1();
 
     std::cout << "\npress any key to exit ..." << std::endl;
     getchar();
